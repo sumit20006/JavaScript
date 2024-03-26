0e6278cd787352769e0a24b61ab8dc912a0cc730
Core <111035840+sumit20006@users.noreply.github.com>
2024-03-26 18:20:36 +0000

    operations done

---
+++ basics1/operations.js
@@ -0,0 +1,30 @@
+let a = 5;
+let b = 10;
+
+let c = a + b; //sum
+console.log(c);
+
+
+let neg_a = -a; //negative of a value
+console.log(neg_a);
+
+
+// console.log(2+2);(add)
+// console.log(2*2);(multiply)
+// console.log(2**2);(power)
+// console.log(2/2);(divide)
+// console.log(2%2);(remainder)
+// console.log(2-2);(minus)
+
+ let str1="sumit";
+ let str2="sahu";
+
+ let str3 = str1 + " " + str2; //adding two strings
+ console.log(str3);
+ //console.log(`${str1} ekjfbesbf@#$123 ${str2}`);
+
+// console.log("1" + 2); // first is string then all are considered string
+// console.log(1 + "2"); // first is considered number and the rest as string
+// console.log("1" + "2"); // both are considered string
+// console.log("1" + 2 + 2); // first string then all string
+// console.log(1 + 2 + "2"); // first two are added then the third is string
